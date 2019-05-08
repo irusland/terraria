9fa3f9d3db075a651e2e6bb6c44593f9314242ad
ruslansir <ruslansir@icloud.com>
2019-05-08 10:41:43 +0500

    generator started

---
+++ terraria/WorldGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace terraria
+{
+    public class WorldGenerator
+    {
+        public World Generate(int width, int height)
+        {
+            var stringMap = new[] {
+                ""
+            };
+            var stringLineMap = new StringBuilder();
+            for (var x = 0; x < width; x++)
+            {
+                for (var y = 0; y < height; y++)
+                {
+                    // TODO Generator
+                }
+            }
+            return World.Create(stringMap);
+        }
+    }
+}
